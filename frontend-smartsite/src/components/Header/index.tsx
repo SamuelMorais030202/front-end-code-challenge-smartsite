75d113c54c008246f82fddd8bc3924f39acddc4e
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-10-19 18:11:42 -0300

    feat: finalizando a construção do header

--- frontend-smartsite/src/components/Header/index.tsx
+++ frontend-smartsite/src/components/Header/index.tsx
@@ -3,7 +3,16 @@ import styled from './header.module.css';
 
 const Header = () => (
   <header className={styled.header}>
-    <img src={ logo } alt="Logo smartfit" />
+    <div className={styled.logoContainer}>
+      <img src={ logo } alt="Logo smart fit" />
+    </div>
+    <div className={styled.textContainer}>
+      <div className={styled.heading}>
+        <h1>REABERTURA <br /> SMART FIT</h1>
+        <span className={styled.headingBorder}></span>
+      </div>
+      <p>O horário de funcionamento das nossas unidades está seguindo os decretos de cada município. Por isso, confira aqui se a sua unidade está aberta e as medidas de segurança que estamos seguindo.</p>
+    </div>
   </header>
 );
 
