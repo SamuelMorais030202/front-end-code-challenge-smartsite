d547ae39ed91b1ed3b4b25fd1d466a546e8be6b8
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-10-21 14:25:49 -0300

    feat: adicionando alt as imagens

--- frontend-smartsite/src/components/Legend/index.tsx
+++ frontend-smartsite/src/components/Legend/index.tsx
@@ -7,12 +7,12 @@ const Legend = () => {
         <h3>Máscara</h3>
         <div className={styled.legendImages}>
           <div className={styled.legendImage}>
-            <img src="/required-mask.png" alt="" />
+            <img src="/required-mask.png" alt="image required mask" />
             <p>Obrigatório</p>
           </div>
 
           <div className={styled.legendImage}>
-            <img src="/recommended-mask.png" alt="" />
+            <img src="/recommended-mask.png" alt="image recommended mask" />
             <p>Recomendado</p>
           </div>
 
@@ -23,12 +23,12 @@ const Legend = () => {
         <h3>Toalha</h3>
         <div className={styled.legendImages}>
           <div className={styled.legendImage}>
-            <img src="/required-towel.png" alt="" />
+            <img src="/required-towel.png" alt="image required towel" />
             <p>obrigatório</p>
           </div>
 
           <div className={styled.legendImage}>
-            <img src="/recommended-towel.png" alt="" />
+            <img src="/recommended-towel.png" alt="image recommended towel" />
             <p>Recomendado</p>
           </div>
   
@@ -39,12 +39,12 @@ const Legend = () => {
         <h3>Bebedouro</h3>
         <div className={styled.legendImages}>
           <div className={styled.legendImage}>
-            <img src="/partial-fountain.png" alt="" />
+            <img src="/partial-fountain.png" alt="image partial fountain" />
             <p>Parcial</p>
           </div>
 
           <div className={styled.legendImage}>
-            <img src="/forbidden-fountain.png" alt="" />
+            <img src="/forbidden-fountain.png" alt="image forbidden fountain" />
             <p>Proibido</p>
           </div>
 
@@ -55,17 +55,17 @@ const Legend = () => {
         <h3>Vestiários</h3>
         <div className={styled.legendImages}>
           <div className={styled.legendImage}>
-            <img src="/required-lockerroom.png" alt="" />
+            <img src="/required-lockerroom.png" alt="image required lockerroum" />
             <p>Liberado</p>
           </div>
 
           <div className={styled.legendImage}>
-            <img src="/partial-lockerroom.png" alt="" />
+            <img src="/partial-lockerroom.png" alt="image partial lockerrom" />
             <p>Parcial</p>
           </div>
 
           <div className={styled.legendImage}>
-            <img src="/forbidden-lockerroom.png" alt="" />
+            <img src="/forbidden-lockerroom.png" alt="image forbidden lockerrom" />
             <p>Fechado</p>
           </div>
         </div>
